a88d53d30c681051b903e5ec8cca112aa7c4ffb8
Danny Gonzalez <danny.galicia@rhizom.me>
2022-08-05 05:43:27 -0300

    add adminClient Proposal

--- src/OpenzeppelinDefender/adminClient/index.ts
+++ src/OpenzeppelinDefender/adminClient/index.ts
@@ -1,4 +1,4 @@
-import { AdminClient, Contract  } from 'defender-admin-client';
+import { AdminClient, Contract,  } from 'defender-admin-client';
 import { credentialClient } from '../models/type';
 
 export default class AdminClients {
@@ -11,5 +11,20 @@ export default class AdminClients {
 
   public addContract(Contract:Contract) {
     return this.client.addContract(Contract);
+  }
+  public createProposal(ProposalResponseWithUrl:any) {
+    return this.client.createProposal(ProposalResponseWithUrl);
+  }
+
+  public proposeUpgrade(UpgradeParams:any,contract:any) {
+    return this.client.proposeUpgrade(UpgradeParams,contract);
+  }
+
+  public proposeUnpause(PauseParams:any,contract:any) {
+    return this.client.proposeUnpause(PauseParams,contract);
+  }
+
+  public listProposals(PauseParams:any,contract:any) {
+    return this.client.listProposals();
   }
 }
