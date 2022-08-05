60dabf9ddff53a0336c437649340a7ef74ce0486
Danny Gonzalez <danny.galicia@rhizom.me>
2022-08-05 13:50:19 -0300

    fix bug

--- test/project.test.ts
+++ test/project.test.ts
@@ -6,10 +6,8 @@ describe("Integration tests examples", function () {
 
     it("Should add the example field",async function () {
      
-       await this.hre.OpenzeppelinDefender.KvstoreClient.put("hola",JSON.stringify({loca:'loca'}));
-       const result:any =await this.hre.OpenzeppelinDefender.KvstoreClient.get("hola")
-       const r= JSON.parse(result);
-       console.log(r.loca)
+       
+       console.log(await this.hre.OpenzeppelinDefender.AutoTaskClint.list())
     });
 
    
